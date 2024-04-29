9e0b6a6b14b7bb704d0388182bd8dac5670d33dc
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-29 19:03:27 +0300

    dd

--- bin/chat_qr.dart
+++ bin/chat_qr.dart
@@ -1,40 +1,35 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
-class User {
-  late WebSocket webSocket;
-  late String chatId;
-
-  User(this.webSocket, this.chatId);
-}
-
-List<User> users = [];
+List<Map<String, dynamic>> users = [];
 List<Map<String, dynamic>> messages = [];
 
 void main() {
-  var handler = webSocketHandler((webSocketChannel) {
-    // Приводим WebSocketChannel к типу WebSocket
-    var webSocket = webSocketChannel.cast<WebSocket>();
+  var handler = webSocketHandler((webSocket) {
     webSocket.stream.listen((message) {
+      // При получении сообщения от клиента
       var parsedMessage = jsonDecode(message);
 
+      // Если сообщение содержит информацию о новом пользователе
       if (parsedMessage.containsKey('action') && parsedMessage['action'] == 'join') {
-        var chatId = parsedMessage['chatId'] as String;
-        var user = User(webSocket, chatId);
-        users.add(user);
-        var chatMessages = messages.where((msg) => msg['cid'] == chatId).toList();
-        user.webSocket.add(jsonEncode(chatMessages));
+        // Добавляем пользователя в список
+        users.add({
+          'webSocket': webSocket,
+          'cid': parsedMessage['cid'],
+          // Можно также сохранить какие-то другие данные о пользователе
+        });
       } else {
+        // Добавляем сообщение в список
         messages.add(parsedMessage);
 
+        // Отправляем сообщение всем подписчикам, кроме отправителя
         for (var user in users) {
-          if (user.webSocket == webSocket && parsedMessage['cid'] == user.chatId) {
-            user.webSocket.add(jsonEncode([parsedMessage]));
+            // Фильтруем сообщения для отправки только тем, кто находится в одном чате
+            var sortedMessages = messages.where((m) => m['cid'] == parsedMessage['cid']).toList();
+            user['webSocket'].sink.add(jsonEncode(sortedMessages));
           }
-        }
       }
     });
   });
