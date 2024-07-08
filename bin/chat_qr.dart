24d2b8309735ad08ccd097d41a298b8f9f978738
SmartCherry <jekcatpopov@gmail.com>
2024-07-09 02:57:31 +0300

    dd

--- bin/chat_qr.dart
+++ bin/chat_qr.dart
@@ -141,6 +141,7 @@ void httpServer(MySQLConnection sql) async {
         return Response.ok(jsonEncode({'chat_id': idInt + 1}));
       }
       else if (data['type'] == 1 || data['type'] == '1') {
+        print('type: 1');
         var resul = await sql.execute(
           "SELECT * FROM chats",
         );
